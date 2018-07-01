65a21a98b19b4f0eb3498597025d13e6d46c7c12
Stefano Pernat <stefano.pernat@gmail.com>
2018-07-01 17:43:38 +0200

    Implement answerPressed

--- Quizzler/QuizzlerViewController.swift
+++ Quizzler/QuizzlerViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class QuizzlerViewController: UIViewController {
     
     //Place your instance variables here
+    let allQuestions = QuestionBank()
+    var pickedAnswer: Bool = false
     
     
     @IBOutlet weak var questionLabel: UILabel!
@@ -21,11 +23,18 @@ class QuizzlerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        questionLabel.text = allQuestions.list[0].questionText
     }
 
 
     @IBAction func answerPressed(_ sender: AnyObject) {
-  
+        if sender.tag == 1 {
+            pickedAnswer = true
+        } else if sender.tag == 2 {
+            pickedAnswer = false
+        }
+        
+        checkAnswer()
     }
     
     
